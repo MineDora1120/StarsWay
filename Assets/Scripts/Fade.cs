5d14f66cb9b5fcdc9842d74965c765cbf441ccc4
MADO <mydodo1120@naver.com>
2023-10-22 16:04:58 +0900

    전환 추가

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
+    static SpriteRenderer BlackScreenRenderer;
+    static Color BlackScreenColor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BlackScreenRenderer = GetComponent<SpriteRenderer>();
+        BlackScreenColor = BlackScreenRenderer.color;
+        BlackScreenRenderer.enabled = true;
+        StartCoroutine(FadeIn());
     }
 
     // Update is called once per frame
-    void Update()
+    static public IEnumerator FadeOut()
     {
-        
+        BlackScreenColor.a = 0;
+        BlackScreenRenderer.enabled = true;
+        for (float i = 0; i < 1f; i += 0.01f)
+        {
+            BlackScreenColor.a = i;
+            BlackScreenRenderer.color = BlackScreenColor;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    static public IEnumerator FadeIn()
+    {
+        for (float i = 1f; i > 0; i -= 0.01f)
+        {
+            BlackScreenColor.a = i;
+            BlackScreenRenderer.color = BlackScreenColor;
+            yield return new WaitForSeconds(0.01f);
+        }
+        BlackScreenRenderer.enabled = false;
     }
 }
