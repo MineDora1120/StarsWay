873dc50760bb09a1bdb13b5cb88d4fb3d40930fa
마도 <mydodo1120@naver.com>
2023-10-27 16:27:04 +0900

    점프애니메이션구현

--- Assets/Scripts/MainGame/StageSceneManager.cs
+++ Assets/Scripts/MainGame/StageSceneManager.cs
@@ -1,20 +1,19 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class StageSceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float delayTime = 5f;
     void Start()
     {
         Application.targetFrameRate = 60;
+        StartCoroutine(Fade.FadeIn(0));
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
