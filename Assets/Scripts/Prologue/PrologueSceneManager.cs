ec2baaed61d3d2c4e5cafc7affc5168aaa17574b
MADO <mydodo1120@naver.com>
2023-10-28 10:37:43 +0900

    오류 안난 버전으로 복

---
+++ Assets/Scripts/Prologue/PrologueSceneManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrologueSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float delayTime = 5f;
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/TextTyping.cs
+++ Assets/Scripts/TextTyping.cs
@@ -4,14 +4,17 @@ using UnityEngine.UI;
 
 public class TextTyping : MonoBehaviour
 {
+    public float letterDelay = 0.1f, AfterDelay = 2;
     Text textObject;
+    string[] texts = new string[] { "평화롭던 어느날...", "이상현은 도트르ㅜㄹ 존나잘짠다" };
 
     private void Start()
     {
         textObject = GetComponent<Text>();
+        StartCoroutine(Printf(AfterDelay, letterDelay));
     }
 
-    public IEnumerator Printf(float After, float Before, string[] texts) //텍스트, 지연시간, 텍스트 오브젝트
+    IEnumerator Printf(float After, float Before) //텍스트, 지연시간, 텍스트 오브젝트
     {
         for(int i = 0; i < texts.Length; i++)
         {
@@ -20,7 +23,7 @@ public class TextTyping : MonoBehaviour
         }
     }
 
-    public IEnumerator ShowText(string fullText, float letterDelay) //텍스트, 지연시간, 텍스트 오브젝트
+    IEnumerator ShowText(string fullText, float letterDelay) //텍스트, 지연시간, 텍스트 오브젝트
     {
         textObject.text = "";
         for (int i = 0; i <= fullText.Length; i++)
