ff195e669d833a0e14f523b2edd1a0bd4b97f020
MADO <mydodo1120@naver.com>
2023-10-28 10:32:18 +0900

    Revert "점프애니메이션구현"
    This reverts commit 873dc50760bb09a1bdb13b5cb88d4fb3d40930fa.

--- Assets/Scripts/Prologue/GameManager.cs
+++ Assets/Scripts/Prologue/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrologueSceneManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public float letterDelay = 0.1f, AfterDelay = 2;
